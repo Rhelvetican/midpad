0f5b5ab50761c627b443ce126c9fd3dd85ad839e
Rh4096 <rhelvetican@gmail.com>
2024-08-23 14:25:11 +0700

    feat: add pad modes

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
-use clap::Parser;
+use clap::{builder::OsStr, Parser, ValueEnum};
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(name = "midpad", version, about = "Command line utility to pad texts.", long_about = None)]
 pub struct Cli {
     #[arg(short, long)]
     pub raw: Option<String>,
@@ -11,4 +11,36 @@ pub struct Cli {
     pub file: Option<PathBuf>,
     #[arg(short, long)]
     pub output: Option<PathBuf>,
+    #[arg(short, long, default_value = PadMode::Middle)]
+    pub mode: Option<PadMode>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, ValueEnum, Default)]
+pub enum PadMode {
+    Left,
+    Right,
+    #[default]
+    Middle,
+}
+
+impl FromStr for PadMode {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "l" | "left" => Ok(PadMode::Left),
+            "m" | "middle" => Ok(PadMode::Left),
+            "r" | "right" => Ok(PadMode::Left),
+            _ => Err(()),
+        }
+    }
+}
+
+impl From<PadMode> for OsStr {
+    fn from(value: PadMode) -> Self {
+        match value {
+            PadMode::Middle => OsStr::from("m"),
+            PadMode::Left => OsStr::from("l"),
+            PadMode::Right => OsStr::from("r"),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,68 @@
+/// Create a new `Vec<String>` that has been middle-padded.
 pub fn midpad(input: Vec<String>) -> Vec<String> {
     let maxlen = input.iter().map(|s| s.len()).max().unwrap_or(0);
 
-    input.into_iter().map(|s| pad(s.trim(), maxlen)).collect()
+    input
+        .into_iter()
+        .map(|s| _midpad(s.trim(), maxlen))
+        .collect()
 }
 
+/// Modify a mutable slice of `Strings` in-place to make it middle-padded.
 pub fn midpad_in_place(input: &mut [String]) {
     let maxlen = input.iter().map(|s| s.len()).max().unwrap_or(0);
 
-    input.iter_mut().for_each(|s| *s = pad(s.trim(), maxlen));
+    input
+        .iter_mut()
+        .for_each(|s| *s = _midpad(s.trim(), maxlen));
 }
 
+/// Create a new `Vec<String>` that has been left-padded.
+pub fn leftpad(input: Vec<String>) -> Vec<String> {
+    input.into_iter().map(|s| s.trim().to_string()).collect()
+}
+
+/// Modify a mutable slice of `Strings` in-place to make it left-padded.
+pub fn leftpad_in_place(input: &mut [String]) {
+    input.iter_mut().for_each(|s| *s = s.trim().to_string());
+}
+
+/// Create a new `Vec<String>` that has been right-padded.
+pub fn rightpad(input: Vec<String>) -> Vec<String> {
+    let maxlen = input.iter().map(|s| s.len()).max().unwrap_or(0);
+
+    input
+        .into_iter()
+        .map(|s| _rightpad(s.trim(), maxlen))
+        .collect()
+}
+
+/// Modify a mutable slice of `Strings` in-place to make it right-padded.
+pub fn rightpad_in_place(input: &mut [String]) {
+    let maxlen = input.iter().map(|s| s.len()).max().unwrap_or(0);
+
+    input
+        .iter_mut()
+        .for_each(|s| *s = _rightpad(s.trim(), maxlen));
+}
+
+/// Generate a string with only spaces.
 fn space(n: usize) -> String {
     " ".repeat(n)
 }
 
-fn pad(src: &str, len: usize) -> String {
+#[doc(hidden)]
+fn _midpad(src: &str, len: usize) -> String {
     let srclen = src.len();
 
     let half = (len - srclen) / 2;
 
-    space(half) + src + &space(half)
+    space(half) + src
+}
+
+#[doc(hidden)]
+fn _rightpad(src: &str, len: usize) -> String {
+    let srclen = src.len();
+
+    space(len - srclen) + src
 }
